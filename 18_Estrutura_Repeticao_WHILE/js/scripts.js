671701a926898aac4d51045a5169b1543c60d9f3
Lucas al Jalali <lucasaljalali@gmail.com>
2022-07-23 22:07:10 +0100

    Learning JS

--- 18_Estrutura_Repeticao_WHILE/js/scripts.js
+++ 18_Estrutura_Repeticao_WHILE/js/scripts.js
@@ -34,3 +34,204 @@ while(i <= 4) {
     i += 1;
 
 }
+
+
+
+
+
+
+
+
+
+const log = console.log;
+
+var x = 0;
+
+while(x <= 5) {
+    
+    log('Testanto repetição ' + x);
+
+    //incrementador
+
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+
+    log(arr[y]);
+
+    y++;
+
+}
+
+
+var palavra = 'Lucas';
+var i = 0;
+
+while(i <= 4) {
+
+    log(palavra[i]);
+
+    i += 1;
+
+}
+
+
+
+
+const log = console.log;
+
+var x = 0;
+
+while(x <= 5) {
+    
+    log('Testanto repetição ' + x);
+
+    //incrementador
+
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+
+    log(arr[y]);
+
+    y++;
+
+}
+
+
+var palavra = 'Lucas';
+var i = 0;
+
+while(i <= 4) {
+
+    log(palavra[i]);
+
+    i += 1;
+
+}
+
+
+
+
+const log = console.log;
+
+var x = 0;
+
+while(x <= 5) {
+    
+    log('Testanto repetição ' + x);
+
+    //incrementador
+
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+
+    log(arr[y]);
+
+    y++;
+
+}
+
+
+var palavra = 'Lucas';
+var i = 0;
+
+while(i <= 4) {
+
+    log(palavra[i]);
+
+    i += 1;
+
+}
+
+
+const log = console.log;
+
+var x = 0;
+
+while(x <= 5) {
+    
+    log('Testanto repetição ' + x);
+
+    //incrementador
+
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+
+    log(arr[y]);
+
+    y++;
+
+}
+
+
+var palavra = 'Lucas';
+var i = 0;
+
+while(i <= 4) {
+
+    log(palavra[i]);
+
+    i += 1;
+
+}
+
+
+const log = console.log;
+
+var x = 0;
+
+while(x <= 5) {
+    
+    log('Testanto repetição ' + x);
+
+    //incrementador
+
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {
+
+    log(arr[y]);
+
+    y++;
+
+}
+
+
+var palavra = 'Lucas';
+var i = 0;
+
+while(i <= 4) {
+
+    log(palavra[i]);
+
+    i += 1;
+
+}
