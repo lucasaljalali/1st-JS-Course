d38d66fcf4d9395cef23309c42c0039a07829d0d
Lucas al Jalali <lucasaljalali@gmail.com>
2022-07-24 20:22:02 +0100

    Learning JS

--- 12_operadores_de_comparacao/js/scripts.js
+++ 12_operadores_de_comparacao/js/scripts.js
@@ -67,3 +67,222 @@ showInfoLog();
 
 
 
+
+
+
+
+
+
+
+const log = console.log
+
+var idade = 18;
+var possuiCarro = 1;
+
+if(idade >=18) {
+    log("Pode fazer carteira");
+} else {
+    log("Não pode fazer carteira")
+}
+
+if(idade <18) {
+    log("Não pode fazer carteira");
+}
+
+if(possuiCarro) {
+    log("Pode andar de carro");
+}
+
+var nome = "Pedro";
+
+if(nome == "Lucas") {
+    log("O nome é Lucas");
+}
+
+if(nome != "Lucas") {
+    log("O nome não é Lucas");
+}
+
+if(20 > 10) {
+    log("Passou");
+}
+
+if(10 < 20) {
+    log("Passou 2");
+}
+
+
+//brincando
+var obj = {
+    nome: undefined,
+    idade: undefined,
+    possuiCarro: undefined,
+};
+
+function showInfoLog() {
+    if (obj.idade >= 18) {
+        log(obj.nome + " pode tirar carteira");
+    } else {
+        log(obj.nome + " não pode tirar carteira")
+    }
+
+    if (obj.possuiCarro) {
+        log(obj.nome + " pode andar de carro");
+    } else {
+        log(obj.nome + " não pode andar de carro")
+        
+    }
+}
+
+obj.nome = "Luna";
+obj.idade = 1;
+obj.possuiCarro = 0; 
+
+showInfoLog();
+
+
+
+
+
+
+
+
+const log = console.log
+
+var idade = 18;
+var possuiCarro = 1;
+
+if(idade >=18) {
+    log("Pode fazer carteira");
+} else {
+    log("Não pode fazer carteira")
+}
+
+if(idade <18) {
+    log("Não pode fazer carteira");
+}
+
+if(possuiCarro) {
+    log("Pode andar de carro");
+}
+
+var nome = "Pedro";
+
+if(nome == "Lucas") {
+    log("O nome é Lucas");
+}
+
+if(nome != "Lucas") {
+    log("O nome não é Lucas");
+}
+
+if(20 > 10) {
+    log("Passou");
+}
+
+if(10 < 20) {
+    log("Passou 2");
+}
+
+
+//brincando
+var obj = {
+    nome: undefined,
+    idade: undefined,
+    possuiCarro: undefined,
+};
+
+function showInfoLog() {
+    if (obj.idade >= 18) {
+        log(obj.nome + " pode tirar carteira");
+    } else {
+        log(obj.nome + " não pode tirar carteira")
+    }
+
+    if (obj.possuiCarro) {
+        log(obj.nome + " pode andar de carro");
+    } else {
+        log(obj.nome + " não pode andar de carro")
+        
+    }
+}
+
+obj.nome = "Luna";
+obj.idade = 1;
+obj.possuiCarro = 0; 
+
+showInfoLog();
+
+
+
+
+
+
+const log = console.log
+
+var idade = 18;
+var possuiCarro = 1;
+
+if(idade >=18) {
+    log("Pode fazer carteira");
+} else {
+    log("Não pode fazer carteira")
+}
+
+if(idade <18) {
+    log("Não pode fazer carteira");
+}
+
+if(possuiCarro) {
+    log("Pode andar de carro");
+}
+
+var nome = "Pedro";
+
+if(nome == "Lucas") {
+    log("O nome é Lucas");
+}
+
+if(nome != "Lucas") {
+    log("O nome não é Lucas");
+}
+
+if(20 > 10) {
+    log("Passou");
+}
+
+if(10 < 20) {
+    log("Passou 2");
+}
+
+
+//brincando
+var obj = {
+    nome: undefined,
+    idade: undefined,
+    possuiCarro: undefined,
+};
+
+function showInfoLog() {
+    if (obj.idade >= 18) {
+        log(obj.nome + " pode tirar carteira");
+    } else {
+        log(obj.nome + " não pode tirar carteira")
+    }
+
+    if (obj.possuiCarro) {
+        log(obj.nome + " pode andar de carro");
+    } else {
+        log(obj.nome + " não pode andar de carro")
+        
+    }
+}
+
+obj.nome = "Luna";
+obj.idade = 1;
+obj.possuiCarro = 0; 
+
+showInfoLog();
+
+
+
